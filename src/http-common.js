d3a4140fb4088b4008ad07e85ce613940cee8850
Gustavo Rodriguez Liccioni <gustavo.liccioni@weareadaptive.com>
2022-06-18 21:49:27 +0200

    fix: adding auth headers

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,7 @@
 import axios from "axios";
+import { AuthHeader } from "./_spjwt/_sphelpers";
 
 export default axios.create({
     baseUrl: "http://localhost:3000",
-    headers: {
-        "Content-type": "application/json"
-    }
+    headers: { ...{ "Content-type": "application/json" }, ...AuthHeader() }
 })
