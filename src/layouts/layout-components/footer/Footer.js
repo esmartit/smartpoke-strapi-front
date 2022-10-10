12391df3497f6c767de15bbcfd6672a5e71552fb
Adolfo <adolfo.zignago@esmartit.es>
2022-10-10 18:33:49 +0200

    fix: LastCommit

--- src/layouts/layout-components/footer/Footer.js
+++ src/layouts/layout-components/footer/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className="footer text-center">
-            All Rights Reserved by Adminpro. Designed and Developed by{' '}
+            All Rights Reserved by Adminpro. SmartPoke Designed and Developed by{' '}
             <a href="https://www.esmartit.es" target="_blank" rel="noopener noreferrer">eSmartIT</a>.
         </footer>
     );
